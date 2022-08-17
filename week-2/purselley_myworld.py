e1a2392122033c29216601e4a38664cf70accadd
Gabe <g19purselley@gmail.com>
2022-08-17 18:20:21 +0900

    myWorld file

---
+++ week-2/purselley_myworld.py
@@ -0,0 +1,9 @@
+""" 
+    Title: purselley_myworld.py
+    Author: Danial Purselley
+    Date: 17 August 2022
+    Description: Beginner level python assignment
+"""
+
+
+print("You are now in Purselley's World")
